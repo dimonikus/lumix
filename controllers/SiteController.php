06b89d2c7cddcb2de50e61b016a541300e9bec7a
dimonikus <dmitry.akulov@gmail.com>
2017-11-23 04:22:09 +0200

    service

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\modules\admin\models\Service;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
@@ -34,7 +35,10 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $model = new Service();
+        $model->index = 5;
+
+        return $this->render('index', ['model' => $model]);
     }
 
 
--- modules/admin/views/service/index.php
+++ modules/admin/views/service/index.php
@@ -1,8 +1,25 @@
 <?php
 /* @var $this yii\web\View */
+use kartik\sortinput\SortableInput;
 ?>
 <h1>service/index</h1>
 
 <p>
     <code>http://demos.krajee.com/sortable-input</code>
 </p>
+
+<?php
+$form = \yii\widgets\ActiveForm::begin();
+echo SortableInput::widget([
+    'name'=> 'sort_list_1',
+    'items' => [
+        1 => ['content' => '<i class="glyphicon glyphicon-cog"></i> Item # 1'],
+        2 => ['content' => '<i class="glyphicon glyphicon-cog"></i> Item # 2'],
+        3 => ['content' => '<i class="glyphicon glyphicon-cog"></i> Item # 3'],
+        4 => ['content' => '<i class="glyphicon glyphicon-cog"></i> Item # 4'],
+        5 => ['content' => '<i class="glyphicon glyphicon-cog"></i> Item # 5']
+    ],
+    'hideInput' => true,
+]);
+\yii\widgets\ActiveForm::end();
+?>
