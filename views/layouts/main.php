85104e1f06b70562e571761857d0e71fda284792
dimonikus <dmitry.akulov@gmail.com>
2018-06-02 00:49:26 +0300

    contact

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -3,12 +3,8 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use app\widgets\Alert;
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
+use yii\helpers\Html;
 use yii\helpers\Url;
 
 AppAsset::register($this);
@@ -249,6 +245,33 @@ $contact = \app\models\ContactPage::find()->one();
     </div>
 </div>
 <a href="#0" class="cd-top" title="Go to top">Top</a>
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+    (function () {
+        var widget_id = 'j347mDSHts';
+        var d = document;
+        var w = window;
+
+        function l() {
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = '//code.jivosite.com/script/widget/' + widget_id;
+            var ss = document.getElementsByTagName('script')[0];
+            ss.parentNode.insertBefore(s, ss);
+        }
+
+        if (d.readyState == 'complete') {
+            l();
+        } else {
+            if (w.attachEvent) {
+                w.attachEvent('onload', l);
+            } else {
+                w.addEventListener('load', l, false);
+            }
+        }
+    })();</script>
+<!-- {/literal} END JIVOSITE CODE -->
 <?php $this->endBody() ?>
 </body>
 </html>
--- views/site/contact.php
+++ views/site/contact.php
@@ -15,113 +15,33 @@ $assetPath = Yii::$app->assetManager->getBundle('app\assets\AppAsset', true)->ba
 <?= \app\widgets\HeaderCaptionWidget::widget(['heading_h1' => Yii::t('app', 'Contact')]) ?>
 <div class="space-medium">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 bg-primary pinside40">
+        <div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="contact-info">
-                    <div class="logo mb40">
-                        <a href="<?= Yii::$app->getHomeUrl() ?>">
-                            <img src="<?= $assetPath ?>/images/logo.png" class="img-responsive" alt="">
-                        </a>
-                    </div>
                     <ul class="listnone">
-                        <li><i class="icon-placeholder icon-secondary icon-2x"></i>
+                        <li><i class="icon-placeholder icon-default icon-2x"></i>
                             <address>
                                 <?= $model->getAddress() ?>
                             </address>
                         </li>
-                        <li><i class=" icon-phone-call icon-secondary icon-2x"></i>
+                        <li><i class=" icon-phone-call icon-default icon-2x"></i>
                             <p><?= $model->getPhone() ?></p>
                         </li>
                         <?php if (!empty($model->mail)): ?>
-                        <li><i class="icon-email icon-secondary icon-2x"></i>
+                        <li><i class="icon-email icon-default icon-2x"></i>
                             <p><?= $model->getMail() ?></p>
                         </li>
                         <?php endif; ?>
                         <?php if (!empty($model->opening_hours)): ?>
-                        <li><i class=" icon-clock icon-secondary icon-2x"></i>
+                        <li><i class=" icon-clock icon-default icon-2x"></i>
                             <p><?= $model->getOpeningHours() ?></p>
                         </li>
                         <?php endif; ?>
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 outline pinside40">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <h2><?= Yii::t('app', 'Contact Form') ?></h2>
-                    </div>
-                </div>
-                <div class="row">
-                    <form>
-                        <!-- Textarea -->
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <!-- Text input-->
-                            <div class="form-group">
-                                <label class="control-label" for="name">
-                                    <?= Yii::t('app', 'Name') ?> :
-                                </label>
-                                <span class="d-inline-block tooltip_message"
-                                      tabindex="0" data-toggle="tooltip" id="tt_name">
-                                    <input name="name" type="text" class="form-control" placeholder=" " required>
-                                </span>
-                            </div>
-                        </div>
-                        <!-- Text input-->
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <div class="form-group">
-                                <label class=" control-label" for="email">
-                                    <?= Yii::t('app', 'email') ?> :
-                                </label>
-                                <span class="d-inline-block tooltip_message"
-                                      tabindex="0" data-toggle="tooltip" id="tt_email">
-                                    <input name="email" type="text" class="form-control" placeholder=" " required>
-                                </span>
-                            </div>
-                        </div>
-                        <!-- Text input-->
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <div class="form-group">
-                                <label class=" control-label" for="phone">
-                                    <?= Yii::t('app', 'Phone') ?> :
-                                </label>
-                                <span class="d-inline-block tooltip_message"
-                                      tabindex="0" data-toggle="tooltip" id="tt_phone">
-                                    <input name="phone" type="text" class="form-control" placeholder=" ">
-                                </span>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <div class="form-group">
-                                <label class=" control-label" for="subject">
-                                    <?= Yii::t('app', 'Subject') ?> :
-                                </label>
-                                <input name="subject" type="text" class="form-control" placeholder=" " required>
-                            </div>
-                        </div>
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div class="form-group">
-                                <label class="control-label" for="message">
-                                    <?= Yii::t('app', 'Message') ?> :
-                                </label>
-                                <span class="d-inline-block tooltip_message"
-                                      tabindex="0" data-toggle="tooltip" id="tt_message">
-                                    <textarea class="form-control" name="message" rows="6" placeholder=" "></textarea>
-                                </span>
-                            </div>
-                        </div>
-                        <!-- Button -->
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div class="form-group">
-                                <a href="" id="send_btn" name="send_btn" class="btn btn-default">
-                                    <?= Yii::t('app', 'send message') ?>
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
         </div>
-        <div class="row">
+        <div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 map" id="googleMap"></div>
         </div>
     </div>
